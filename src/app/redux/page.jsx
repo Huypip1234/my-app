13e6f664991ff5f82eacfcf4b43e828f6e64a831
huypip1234 <huy123nn69hz@gmail.com>
2024-03-12 13:12:42 +0700

    redux

--- src/app/redux/page.jsx
+++ src/app/redux/page.jsx
@@ -1,7 +1,15 @@
+"use client";
+
 import React from "react";
+import { Provider } from "react-redux";
+import { store } from "./store";
 
 const Redux = () => {
-  return <div>page</div>;
+  return (
+    <Provider store={store}>
+      <div>page</div>
+    </Provider>
+  );
 };
 
 export default Redux;
