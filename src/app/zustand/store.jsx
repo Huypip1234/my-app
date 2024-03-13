2010c8e4cfc81b900ceb267694121b777a69719b
Huypip1234 <huy123nn69hz@gmail.com>
2024-03-13 23:39:01 +0700

    ok

---
+++ src/app/zustand/store.jsx
@@ -0,0 +1,8 @@
+import { create } from "zustand";
+
+const useStore = create((set) => ({
+  count: 1,
+  increase: () => set((state) => ({ count: state.count + 1 })),
+}));
+
+export default useStore;
