0d821ecfd284302147fa7bad14e13a3e94d7a455
huypip1234 <huy123nn69hz@gmail.com>
2024-03-18 15:11:46 +0700

    feat: cache

--- src/app/react-query/Basic.jsx
+++ src/app/react-query/Basic.jsx
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react';
 import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query';
 import axios from 'axios';
 import { Button } from '@material-ui/core';
+import Link from 'next/link';
 
 const Basic = () => {
   // Access the client
@@ -10,10 +11,12 @@ const Basic = () => {
   // Queries
   const { isPending, error, data, isFetching } = useQuery({
     queryKey: ['todos'],
+    retry: 1, // goi lai 1 lan neu bi loi (mac dinh 3 lan)
+    retryDelay: 1000, // thoi gian giua cac lan goi lai
+    gcTime: 5 * 60 * 1000, // thoi gian xoa cache: 5 phut (tinh bang miliseconds, default 5 phut)
+    refetchOnWindowFocus: true, // => khi focus vao tab thi se goi lai api (default true)
     queryFn: () =>
-      axios
-        .get('https://jsonplaceholder.typicode.com/todos')
-        .then((res) => res.data),
+      axios.get('http://localhost:3002/todo').then((res) => res.data),
   });
 
   // Mutations;
@@ -28,15 +31,22 @@ const Basic = () => {
     },
   });
 
-  if (isPending) return 'Loading...';
+  if (isPending) return 'Pending...';
 
   if (error) return 'An error has occurred: ' + error.message;
+
+  if (isFetching) {
+    console.log('Fetching...');
+  }
 
   return (
     <div className='form-container flex flex-col items-center my-[1rem] gap-[1rem]'>
       <h1 className='text-[rgb(244,63,94)] font-[600] text-[36px]'>
         React Query
       </h1>
+      <Link href='/react-query/cache-test' className='!text-[rgb(244,63,94)]'>
+        Link
+      </Link>
       <Button
         onClick={() => {
           mutation.mutate({
---
+++ src/app/react-query/cache-test/page.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const CacheTest = () => {
+  return <div>page</div>;
+};
+
+export default CacheTest;
